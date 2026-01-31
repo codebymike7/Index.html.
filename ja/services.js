3ee859e7adaca6058a2e2c5a9e08922445333539
Miguel Benitez <codebymike7@gmail.com>
2026-01-31 10:01:06 -0300

    Create services.js

---
+++ ja/services.js
@@ -0,0 +1,6 @@
+export const services = [
+    { id: 'ind', name: {es: "Industria", en: "Industry"}, icon: "fa-industry", color: "bg-slate-800", link: "industria.html" },
+    { id: 'gas', name: {es: "Gastronomía", en: "Gastronomy"}, icon: "fa-utensils", color: "bg-orange-500", link: "menu.html" },
+    { id: 'bar', name: {es: "Barbería", en: "Barber Shop"}, icon: "fa-scissors", color: "bg-blue-600", link: "turnos.html" },
+    // Puedes seguir agregando todos los que tenías...
+];
